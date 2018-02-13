63b8a9fb6c93c7e9b5bb771965b3e508c0e2ef61
auricless <ciruela.aron@gmail.com>
2018-02-13 13:42:53 +0800

    Add Admin Template

---
+++ admin/events.php
@@ -0,0 +1,46 @@
+    <?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Dashboard
+                            <small>Subheading</small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <table>
+                	<thead>
+                		<tr>
+                			<th>ID</th>
+                			<th>LSG Event Title</th>
+                			<th>LSG Poster</th>
+                			<th>Date</th>
+                		</tr>
+                	</thead>
+                	<tbody>
+                		<?php 
+                			
+                		 ?>
+                	</tbody>
+                </table>
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+   <?php include "includes/admin_footer.php"; ?>
